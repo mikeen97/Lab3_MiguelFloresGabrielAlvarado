0986e8a4a1ebb8db556ab9563954820ea4b2b284
Gabriel Alvarado <gabriel_alvararado24@hotmail.com>
2017-08-04 16:27:56 -0600

    prueba

--- Lab3_MiguelFloresGabrielAlvarado/src/lab3_miguelfloresgabrielalvarado/Lab3_MiguelFloresGabrielAlvarado.java
+++ Lab3_MiguelFloresGabrielAlvarado/src/lab3_miguelfloresgabrielalvarado/Lab3_MiguelFloresGabrielAlvarado.java
@@ -68,7 +68,7 @@ public class Lab3_MiguelFloresGabrielAlvarado {
                     }
                 }
                 JOptionPane.showMessageDialog(null, p1);
-                String puebloActual = JOptionPane.showInputDialog("Ingrese la posicion del pueblo que desea ingresar");
+                int puebloActual = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la posicion del pueblo que desea ingresar"));
                 String opcion2 = "";
                 while (!opcion2.equalsIgnoreCase("e")) {
                     opcion2 = JOptionPane.showInputDialog("Menu\n"
@@ -78,10 +78,146 @@ public class Lab3_MiguelFloresGabrielAlvarado {
                             + "d- Modificar Integrante\n"
                             + "e- Salir \n");
                     if (opcion2.equals("a")) {
-                        String nombre = "", apellido = "", raza = "", acompanante = "", poderAtaque = "", defensa = "", curacion = "";
+                        String nombre = "", apellido = "", raza = "", acompanante = "";
+                        int poderAtaque = 0, defensa = 0, curacion = 0;
                         double altura = 0;
-                        Date fecha;
+                        Date fecha = null;
+                        nombre = JOptionPane.showInputDialog("Ingrese nombre del integrante");
+                        raza = JOptionPane.showInputDialog("Ingrese posicion la raza que desea para el integrante\n"
+                                + "1) elfos\n"
+                                + "2) Enanos\n"
+                                + "3) Hobbits\n"
+                                + "4) Hombres\n"
+                                + "5) Maimar\n"
+                                + "");
+                        apellido = JOptionPane.showInputDialog("Ingrese apellido del integrante");
+                        acompanante = JOptionPane.showInputDialog("Ingrese posicion del acompañante que desea para el integrante\n"
+                                + "1) Aguilas\n"
+                                + "2) Arañas\n"
+                                + "3) Balrogs\n"
+                                + "4) Bestias Aladas\n"
+                                + "5) Dragones\n"
+                                + "");
+                        altura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura de el integrante"));
+                        if (raza.equalsIgnoreCase("1")) {
+                            raza = "elfos";
+                            poderAtaque = 443;
+                            defensa = 150;
+                            curacion = 335;
+                            String arma = JOptionPane.showInputDialog("Ingrese arma del Elfo si/no (Ingrese si o no)");
+                            if (acompanante.equalsIgnoreCase("1")) {
+                                acompanante = "Aguilas";
+                                int contador = 0;
+                                for (pueblo t1 : ListPueblos) {
+                                    if (puebloActual == contador) {
+                                        elfos m = new elfos(arma, raza, nombre, apellido, altura, fecha, acompanante, poderAtaque, defensa, curacion);
+                                        t1.setListIntegrantes((Integrantes) m);
+                                    }
+                                    contador++;
+                                }
+                            }
+                            if (acompanante.equalsIgnoreCase("2")) {
+                                acompanante = "Aranas";
+                                int contador = 0;
+                                for (pueblo t1 : ListPueblos) {
+                                    if (puebloActual == contador) {
+                                        elfos m = new elfos(arma, raza, nombre, apellido, altura, fecha, acompanante, poderAtaque, defensa, curacion);
+                                        t1.setListIntegrantes((Integrantes) m);
+                                    }
+                                    contador++;
+                                }
+                            }
+                            if (acompanante.equalsIgnoreCase("3")) {
+                                acompanante = "Balrogs";
+                                int contador = 0;
+                                for (pueblo t1 : ListPueblos) {
+                                    if (puebloActual == contador) {
+                                        elfos m = new elfos(arma, raza, nombre, apellido, altura, fecha, acompanante, poderAtaque, defensa, curacion);
+                                        t1.setListIntegrantes((Integrantes) m);
+                                    }
+                                    contador++;
+                                }
+                            }
+                            if (acompanante.equalsIgnoreCase("4")) {
+                                acompanante = "Bestias Aladas";
+                                int contador = 0;
+                                for (pueblo t1 : ListPueblos) {
+                                    if (puebloActual == contador) {
+                                        elfos m = new elfos(arma, raza, nombre, apellido, altura, fecha, acompanante, poderAtaque, defensa, curacion);
+                                        t1.setListIntegrantes((Integrantes) m);
+                                    }
+                                    contador++;
+                                }
+                            }
+                            if (acompanante.equalsIgnoreCase("5")) {
+                                acompanante = "Dragones";
+                                int contador = 0;
+                                for (pueblo t1 : ListPueblos) {
+                                    if (puebloActual == contador) {
+                                        elfos m = new elfos(arma, raza, nombre, apellido, altura, fecha, acompanante, poderAtaque, defensa, curacion);
+                                        t1.setListIntegrantes((Integrantes) m);
+                                    }
+                                    contador++;
+                                }
+                            }
+                        }
 
+                        if (raza.equalsIgnoreCase("2")) {
+                            raza = "enanos";
+                            poderAtaque = 300;
+                            defensa = 200;
+                            curacion = 50;
+                            String barba = "No tiene ", hacha = "No tiene";
+                            String pruba_barba = JOptionPane.showInputDialog("Si el enano tiene barba ingrese 1");
+                            String pruba_hacha = JOptionPane.showInputDialog("Si el enano tiene hacha ingrese 1");
+                            if (pruba_barba.equalsIgnoreCase("1")) {
+                                barba = "SitieneBarba";
+                            }
+                            if (pruba_hacha.equalsIgnoreCase("1")) {
+                                hacha = "SitieneHacha";
+                            }
+                            if (acompanante.equalsIgnoreCase("1")) {
+                                acompanante = "Aguilas";
+                                int contador = 0;
+                                for (pueblo t1 : ListPueblos) {
+                                    if (puebloActual == contador) {
+                                        enanos m = new enanos("SiTieneHermano", barba, hacha, raza, nombre, apellido, altura, fecha, acompanante, poderAtaque, defensa, curacion);
+                                        t1.setListIntegrantes((Integrantes) m);
+                                    }
+                                    contador++;
+                                }
+                            }
+                            if (acompanante.equalsIgnoreCase("2")) {
+
+                            }
+                            if (acompanante.equalsIgnoreCase("3")) {
+
+                            }
+                            if (acompanante.equalsIgnoreCase("4")) {
+
+                            }
+                            if (acompanante.equalsIgnoreCase("5")) {
+
+                            }
+                        }
+
+                        if (raza.equalsIgnoreCase("3")) {
+                            if (acompanante.equalsIgnoreCase("1")) {
+                                raza = "maimar";
+                            }
+                        }
+
+                        if (raza.equalsIgnoreCase("4")) {
+                            if (acompanante.equalsIgnoreCase("1")) {
+                                raza = "maimar";
+                            }
+                        }
+
+                        if (raza.equalsIgnoreCase("5")) {
+                            if (acompanante.equalsIgnoreCase("1")) {
+                                raza = "maimar";
+                            }
+                        }
                     }
                 }
             }
@@ -108,13 +244,13 @@ public class Lab3_MiguelFloresGabrielAlvarado {
 
                     }
                     if (opcion3.equals("c")) {
-                        String p1 = "";
+                        String p10 = "";
                         for (Lugares t1 : ListLugares) {
                             if (t1 instanceof Lugares) {
-                                p1 += ListPueblos.indexOf(t1) + " " + ((Lugares) t1) + "\n";
+                                p10 += ListPueblos.indexOf(t1) + " " + ((Lugares) t1) + "\n";
                             }
                         }
-                        JOptionPane.showMessageDialog(null, p1);
+                        JOptionPane.showMessageDialog(null, p10);
                     }
                     if (opcion.equals("d")) {
                         int pos = Integer.parseInt(
@@ -135,6 +271,5 @@ public class Lab3_MiguelFloresGabrielAlvarado {
                 }
             }
         }
-
     }
 }
